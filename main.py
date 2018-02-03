07921417e140236f7e7babbf0b497aeceff7c964
SliinQ <sliinq2012@gmail.com>
2018-02-03 20:29:24 +0100

    Cleaned up the repo, moved all code into one file
    Support for both videos and images, works with specific beacon image.
    Doesn't require specific colors, uses greyscale.
    It has a 75% success rate with the included 'beacon2.0.mp4' test video.

---
+++ main.py
@@ -0,0 +1,216 @@
+import argparse
+import imutils
+import cv2
+import time
+import sys
+
+
+min_area = 200
+# area_range = (6.5, 8.5)
+area_range = (10.8, 12.2)
+
+debug = False
+out = None
+
+
+def show(im, win='img', wait=0):
+    cv2.imshow(win, im)
+    key = cv2.waitKey(wait) & 0xFF
+
+    if key == ord('s'):
+        # Save this frame
+        cv2.imwrite('saves/' + str(int(time.time())) + '.jpg', im)
+        print('frame saved.')
+    elif key == ord('q'):
+        sys.exit(0)
+
+
+def hasVertices(comp, vertices):
+    c = cv2.convexHull(comp[0])
+    # c = comp[0]
+
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.05 * peri, True)
+
+    if len(approx) == vertices:
+        return True
+    return False
+
+
+def hasChild(comp):
+    h = comp[1]
+
+    if h[2] < 0:
+        return False
+    return True
+
+
+def hasParent(comp):
+    h = comp[1]
+
+    if h[3] < 0:
+        return False
+    return True
+
+
+# DEPRECATED
+def findNextShape(comp, comps):
+    c = cv2.convexHull(comp[0])
+    area_old = cv2.contourArea(c)
+
+    if hasParent(comp):
+        p = comps[comp[1][3]]
+        area_parent = cv2.contourArea(p[0])
+
+        if area_parent/area_old < 1.1:
+            return findNextShape(p, comps)
+        else:
+            return p
+    return None
+
+
+def findBeacon(comps):
+    for comp in comps:
+        if hasVertices(comp, 3) and not hasChild(comp) and hasParent(comp):
+            c = cv2.convexHull(comp[0])
+            area_triangle = cv2.contourArea(c)
+
+            area_outer = 0
+            parent = comp
+            while not (area_range[0] < (area_outer/area_triangle) < area_range[1]) and hasParent(parent):
+                # parent = findNextShape(parent, comps)
+                parent = comps[parent[1][3]]
+                if not parent:
+                    break
+
+                area_outer = cv2.contourArea(parent[0])
+
+            if area_range[0] < (area_outer/area_triangle) < area_range[1]:
+                return parent
+    return None
+
+
+def checkFrame(frame):
+    frame = imutils.resize(frame, width=800)
+
+    height, width, channels = frame.shape
+    centerX = int(width/2)
+    centerY = int(height/2)
+
+    frame = cv2.GaussianBlur(frame, (5, 5), 0)
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # if debug:
+    #     show(grey)
+
+    mask = cv2.Canny(grey, 15, 70)
+    if debug:
+        show(mask)
+
+    mask = cv2.dilate(mask, None, iterations=1)
+    mask = cv2.erode(mask, None, iterations=1)
+    if debug:
+        show(mask)
+
+    __, cnts, hierarchy = cv2.findContours(mask, cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+
+    # TMP Center Circle
+    # cv2.circle(frame, (centerX, centerY), 10, (255, 255, 255), -1)
+    try:
+        comps = list(zip(cnts, hierarchy[0]))
+    except TypeError:
+        return 0, -1
+
+    beacon = findBeacon(comps)
+    if beacon:
+        cv2.drawContours(frame, [beacon[0]], -1, (0, 255, 0), 2)
+        M = cv2.moments(beacon[0])
+        try:
+            cX = int(M['m10'] / M['m00'])
+            cY = int(M['m01'] / M['m00'])
+        except ZeroDivisionError:
+            print('ZeroDivide')
+
+        cv2.circle(frame, (cX, cY), 7, (255, 255, 255), -1)
+
+        text = ''
+
+        if cY < centerY:
+            # Above center
+            text += 'Top '
+        else:
+            # Below center
+            text += 'Bottom '
+
+        if cX < centerX:
+            # Left of center
+            text += 'Left.'
+        else:
+            # Right of center
+            text += 'Right.'
+
+        cv2.putText(frame, text, (20, height-40),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    else:
+        # print('NONE')
+        # show(frame)
+        # show(mask)
+        # for cnt in cnts:
+        #     cv2.drawContours(frame, [cnt], -1, (255, 0, 0), 1)
+        #     show(frame)
+        return frame, -1
+    return frame, 0
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-d', '--debug', action='store_true',
+                    help='enable some debugging information')
+    ap.add_argument('-o', '--outvid', help='output file for video')
+    group = ap.add_mutually_exclusive_group(required=True)
+    group.add_argument('-i', '--image', help='path to a image file')
+    group.add_argument('-v', '--video', help='path to a video file')
+    args = vars(ap.parse_args())
+
+    debug = args['debug']
+
+    if args.get('outvid'):
+        fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+        # out = cv2.VideoWriter(args['outvid'], fourcc, 30.0, (1280, 720))
+        out = cv2.VideoWriter(args['outvid'], fourcc, 30.0, (800, 450))
+        print('activate')
+
+    if args.get('image'):
+        im = cv2.imread(args['image'])
+        im, state = checkFrame(im)
+
+        show(im)
+    elif args.get('video'):
+        frame_count = 0
+        frame_error_count = 0
+        cap = cv2.VideoCapture(args['video'])
+
+        # Loop through vid
+        while (cap.isOpened()):
+            ret, im = cap.read()
+            if not ret:
+                break
+
+            im, state = checkFrame(im)
+            if out:
+                out.write(im)
+
+            if debug:
+                show(im)
+            else:
+                show(im, wait=1)
+
+            frame_count += 1
+            if state < 0:
+                frame_error_count += 1
+
+        print(f'Total played frames: {frame_count}')
+        print(f'Frames without beacon: {frame_error_count}')
+        print(f'Percentage OK: {((frame_count-frame_error_count)/frame_count)*100}%')
+
+    cv2.destroyAllWindows()
